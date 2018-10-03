6ce50f6cc31e5bc3fbc5d5aba3a8c265748d5752
Rahul Sharma <rahul@propertyguru.com.sg>
2018-10-03 17:24:00 +0800

    Removed dirty secrets

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://test:Pgtest123@ds121603.mlab.com:21603/mytest
+#export MONGODB_CREDENTIALS=mongodb://test:Pgtest123@ds121603.mlab.com:21603/mytest
 
 # finally, run the app in dev mode...
 npm run dev
